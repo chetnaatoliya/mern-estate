ed0574624981dd9b0084d193a345e54af94efb89
chetna232 <chetnaatoliya23@gmail.com>
2024-01-24 19:52:12 +0530

    api is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
 import Home from './pages/Home';
 import SingIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -14,9 +14,8 @@ export default function App() {
     <Route path="/" element={<Home />}/>
     <Route path="/sign-in" element={<SingIn />}/>
     <Route path ="/sing-up" element={<SignUp/>}/>
-    <Route path ="/About" element={<About />}/>
-    <Route path ="/Profile" element={<Profile />}/>
+    <Route path ="/about" element={<About />}/>
+    <Route path ="/profile" element={<Profile />}/>
     </Routes>
-    </BrowserRouter>
-  
+    </BrowserRouter>  
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React ,{FaSearch} from 'react-icons/fa';
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 
 export default function Header() {
@@ -16,10 +17,12 @@ export default function Header() {
             <FaSearch className ='text-slate-600' /> 
      </form>
      <ul className='flex gap-4'> 
-     <Link to="/"><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
-     <Link to ="/about"><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-     <Link to ="/sign-in"><li className='text-slate-700 hover:underline'>Sign in</li></Link>
-     </ul> </div>   
+     <Link to="/"><li className=' text-slate-700 hover:underline'>Home</li></Link>
+     <Link to ="/about"><li className=' text-slate-700 hover:underline'>About</li></Link>
+     <Link to ="/sign-in"><li className='text-slate-700 hover:underline'>Sign in</li>
+     </Link>
+     </ul>
+      </div>   
      </header>
   )
 }
